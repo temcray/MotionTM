c9e1034603fe4eef2cbf8d3045aafb4d0aeb2530
Tatiana Mccray <ejamison06@gmail.com>
2026-04-07 19:37:37 -0500

    Initial Commit

---
+++ MotionTM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotionTM
+//
+//  Created by Tatiana6mo on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotionTM/MotionTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionTMApp.swift
+//  MotionTM
+//
+//  Created by Tatiana6mo on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MotionTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
